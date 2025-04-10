7018c02f41fb9f39ea3540e797c67fb8e3463191
Joseph Higaki <josephhigaki@hotmail.com>
2025-04-10 11:48:54 +0200

    Refactor postStartCommand.sh to store multiple export commands in variables and append them to .bashrc

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # filepath: /workspaces/emr_data_pipeline/.devcontainer/postStartCommand.sh
 
+# Store the export commands in variables
+ENV_EXPORT="export \$(grep -v '^#' /workspaces/emr_data_pipeline/.env | xargs)"
+AIRFLOW_ENV_EXPORT="export \$(grep -v '^#' /workspaces/emr_data_pipeline/airflow/.env | xargs)"
+DBT_CREDS_EXPORT="export DBT_DATABASE_CREDENTIALS=\$AIRFLOW_DBT_CREDENTIALS_FILEPATH"
 
-# Store the export command in a variable
-ENV_EXPORT="export $(grep -v '^#' /workspaces/emr_data_pipeline/.env | xargs)"
-# Append the export command to .bashrc only if it doesn't already exist
+# Append the export commands to .bashrc only if they don't already exist
 grep -qF "$ENV_EXPORT" ~/.bashrc || echo "$ENV_EXPORT" >> ~/.bashrc
+grep -qF "$AIRFLOW_ENV_EXPORT" ~/.bashrc || echo "$AIRFLOW_ENV_EXPORT" >> ~/.bashrc
+grep -qF "$DBT_CREDS_EXPORT" ~/.bashrc || echo "$DBT_CREDS_EXPORT" >> ~/.bashrc
 
 # Set the timezone
 sudo ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
