723d4a635ba3410e188829540d58caa3ff262896
Joseph Higaki <josephhigaki@hotmail.com>
2025-04-10 23:14:36 +0200

    Add incremental model for patient data with unique key configuration

---
+++ airflow/dags/dbt/emr_analytics/models/marts/dim_patients.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized='incremental',
+         unique_key='patient_id'         
+    )
+}}
+with
+
+source as (
+
+    select * from {{ ref('int_patients') }}
+
+    {% if is_incremental() %}
+
+    -- this filter will only be applied on an incremental run    
+    where ingested_at > (select coalesce(max(ingested_at), '1800-01-01') from {{ this }})
+
+    {% endif %}
+
+)
+select * from source
