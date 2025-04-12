eb6d1c09ff6eea516bb377645e90b6837b97777c
Joseph Higaki <josephhigaki@hotmail.com>
2025-04-12 20:53:09 +0200

    Refactor timestamp expanding in staging model for medications byusing macro

--- airflow/dags/dbt/emr_analytics/models/staging/emr/stg_emr__medications.sql
+++ airflow/dags/dbt/emr_analytics/models/staging/emr/stg_emr__medications.sql
@@ -17,21 +17,14 @@ renamed as (
         payer_coverage,
         dispenses,
         totalcost as total_cost,
-        start as start_at_string,
-        {{ dbt.safe_cast("start", api.Column.translate_type("timestamp")) }} as start_at,                
-        stop as stop_at_string,
-        {{ dbt.safe_cast("stop", api.Column.translate_type("timestamp")) }} as stop_at,        
+        
+        {{ expand_timestamp_column('start') }},
+        
+        {{ expand_timestamp_column('stop') }},
+
         reasoncode as reason_code,
         reasondescription as reason_description,
         ingested_at
     from source
-),
-date_extracted as (
-    select 
-        *,
-        {{ dbt.safe_cast(date_trunc("day", "start_at"), api.Column.translate_type("date"))  }} as start_date,
-        {{ dbt.safe_cast(date_trunc("day", "stop_at"), api.Column.translate_type("date"))  }} as stop_date
-    from renamed
-
 )
-select * from date_extracted
+select * from renamed
