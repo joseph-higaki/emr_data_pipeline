bde450b3f656d88714dbdc6739481aeebe0af2e5
Joseph Higaki <josephhigaki@hotmail.com>
2025-04-13 11:25:24 +0200

    Add patient readmissions analysis and age group seed data
    - Create patient_readmissions.sql to analyze patient encounters and calculate days between encounters.
    - Update int_patient_encounters.sql to include patient age at encounter start and stop, and encounter duration.
    - Refactor fact_patient_encounters.sql to streamline encounter data retrieval.
    - Add age_group.csv seed data for age categorization.

--- airflow/dags/dbt/emr_analytics/analyses/patient_readmissions.sql
+++ airflow/dags/dbt/emr_analytics/analyses/patient_readmissions.sql
@@ -4,25 +4,37 @@ with encounter as(
     from {{ ref('int_patient_encounters') }}
 
 )
-, encounter_and_past_encounters as ( 
+, result as ( 
     select
-        current_e.*,
+        current_e.patient_id,
+        current_e.start_at,
+        current_e.stop_at,
+        current_e.encounter_id,
+        current_e.organization_id,
+        past_e.encounter_id as past_encounter_id,
+        past_e.start_at as past_start_at,
+        past_e.stop_at as past_stop_at,
+        current_e.encounter_class,
+        current_e.encounter_description,
+        current_e.encounter_code,        
         {{ datediff("past_e.start_at",  "current_e.start_at", "second") }} / 60 /60 /24 as days_between_encounters,
         row_number() over (partition by current_e.encounter_id order by current_e.start_at, past_e.start_at) as past_encounter_number
+
+
     from encounter current_e
     left join encounter past_e     
         on current_e.patient_id = past_e.patient_id
             and current_e.patient_id = past_e.patient_id
             and current_e.organization_id = past_e.organization_id
             and current_e.encounter_class = past_e.encounter_class            
-            and current_e.start_at > past_e.start_at    
-)
-, result as(
-    select * 
-    from encounter_and_past_encounters
-    where past_encounter_number = 1
-)
+            and current_e.start_at > past_e.start_at
+    order by 
+        current_e.patient_id desc,
+        current_e.start_at,
+        past_e.start_at
 
-select
-    *
+    limit 200
+)
+select * 
 from result
+where past_encounter_number = 1
--- airflow/dags/dbt/emr_analytics/models/intermediate/int_patient_encounters.sql
+++ airflow/dags/dbt/emr_analytics/models/intermediate/int_patient_encounters.sql
@@ -23,31 +23,43 @@ patients as (
         patient_id,
         birth_date
     from {{ ref('int_patients') }}
+),
+patients_age_encoutner_duration as (
+    select
+        e.encounter_id,    
+        e.start_at,
+        e.start_date,    
+        e.stop_at,
+        e.stop_date,    
+        {{ datediff("p.birth_date", "e.start_at", "month") }} / 12 as patient_age_at_encounter_start_years,
+        {{ datediff("p.birth_date", "e.stop_at", "month") }} / 12 as patient_age_at_encounter_stop_years,
+        {{ datediff("e.start_at", "e.stop_at", "second") }} /60/60 as encounter_duration_hours,
+        e.patient_id,
+        e.organization_id,
+        e.provider_id,
+        e.payer_id,
+        e.encounter_class,
+        e.encounter_code,
+        e.encounter_description,
+        e.base_encounter_cost,
+        e.total_claim_cost,
+        e.payer_coverage,
+        e.reason_code,
+        e.reason_description,
+        e.ingested_at        
+    from source e
+    left join patients p
+        on e.patient_id = p.patient_id
+),
+encounter_with_age_group as (
+    select 
+        e.*,
+        ag.*
+    from patients_age_encoutner_duration e
+    join {{ ref('age_group')}} ag 
+        on ag.min_age < e.patient_age_at_encounter_start_years 
+        and patient_age_at_encounter_start_years <= ag.max_age        
+    
 )
-select
-    e.encounter_id,    
-    e.start_at,
-    e.start_date,    
-    e.stop_at,
-    e.stop_date,    
-    {{ datediff("p.birth_date", "e.start_at", "month") }} / 12 as patient_age_at_encounter_start_years,
-    {{ datediff("p.birth_date", "e.stop_at", "month") }} / 12 as patient_age_at_encounter_stop_years,
-    {{ datediff("e.start_at", "e.stop_at", "second") }} /60/60 as encounter_duration_hours,
-    e.patient_id,
-    e.organization_id,
-    e.provider_id,
-    e.payer_id,
-    e.encounter_class,
-    e.encounter_code,
-    e.encounter_description,
-    e.base_encounter_cost,
-    e.total_claim_cost,
-    e.payer_coverage,
-    e.reason_code,
-    e.reason_description,
-    e.ingested_at,
-    p.birth_date as patient_birth_date
-from source e
-left join patients p
-    on e.patient_id = p.patient_id
-
+select * 
+from encounter_with_age_group
