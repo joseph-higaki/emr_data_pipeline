1db51ab4b0d8abc6fd36e3052d5f24aa58c49655
Joseph Higaki <josephhigaki@hotmail.com>
2025-04-14 22:55:29 +0200

    Enable execution permissions for local run scripts and terraform tfvars generation

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -13,3 +13,12 @@ grep -qF "$DBT_CREDS_EXPORT" ~/.bashrc || echo "$DBT_CREDS_EXPORT" >> ~/.bashrc
 
 # Set the timezone
 sudo ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+
+# Enable terraform tfvars generation script
+chmod +x /workspaces/emr_data_pipeline/terraform/generate_tfvars.sh
+
+# Enable local run: test/debug ingestion script
+chmod +x  /workspaces/emr_data_pipeline/scripts/emr_ingestion/run_local.sh
+
+# Enable local run: test/debug transformation script
+chmod +x /workspaces/emr_data_pipeline/scripts/emr_transformation/run_local.sh
